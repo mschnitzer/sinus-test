715d430379c99a92092bb737762ad167a39fb2be
Manuel Schnitzer <mschnitzer@suse.de>
2016-01-29 07:33:25 +0100

    More test cases

--- spec/sinus_spec.rb
+++ spec/sinus_spec.rb
@@ -10,5 +10,7 @@ describe "method tests" do
     expect(is_in_front_of_person(20, 100.0)).to eq(false)
     expect(is_in_front_of_person(50, 190.0)).to eq(false)
     expect(is_in_front_of_person(270, 350.0)).to eq(false)
+    expect(is_in_front_of_person(0, 350.0)).to eq(true)
+    expect(is_in_front_of_person(0, 270.0)).to eq(false)
   end
 end
